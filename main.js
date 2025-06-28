b3a5757abead69490eacfbe8ca62611c747af61e
Rumsha-Syed <rumsha@locumsunited.com>
2025-06-28 08:45:01 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,34 @@
-const { Actor } = require('apify');
+import { Actor } from 'apify';
+import { PuppeteerCrawler } from 'crawlee';
 
-Actor.main(async () => {
-    const input = await Actor.getInput();
-    const { startUrl } = input;
+await Actor.init();
 
-    const page = await Actor.createPuppeteerPage();
-    await page.goto(startUrl, { waitUntil: 'networkidle2' });
+const input = await Actor.getInput();
+const { startUrl } = input;
 
-    await page.waitForSelector('[class*="member-name"]');
+const results = [];
 
-    const members = await page.evaluate(() => {
-        const elements = Array.from(document.querySelectorAll('[class*="member-name"]'));
-        return elements.map(el => {
-            const name = el.innerText.trim();
-            const [firstName = '', lastName = ''] = name.split(' ');
-            return { firstName, lastName };
+const crawler = new PuppeteerCrawler({
+    requestHandler: async ({ page }) => {
+        await page.goto(startUrl, { waitUntil: 'networkidle2' });
+        await page.waitForSelector('[class*="member-name"]');
+
+        const members = await page.evaluate(() => {
+            const elements = Array.from(document.querySelectorAll('[class*="member-name"]'));
+            return elements.map(el => {
+                const name = el.innerText.trim();
+                const [firstName = '', lastName = ''] = name.split(' ');
+                return { firstName, lastName };
+            });
         });
-    });
 
-    await Actor.pushData(members);
+        results.push(...members);
+    },
+    maxRequestsPerCrawl: 1,
+    headless: true,
 });
+
+await crawler.run([{ url: startUrl }]);
+
+await Actor.pushData(results);
+await Actor.exit();
